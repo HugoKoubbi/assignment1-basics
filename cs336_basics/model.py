d47aa0203c8331cdd3f95ea460b6a3db0abe34af
Hugo Koubbi <koubbihugo@gmail.com>
2026-09-16 09:44:01 +0200

    put ablation studies

--- cs336_basics/model.py
+++ cs336_basics/model.py
@@ -4,6 +4,9 @@ import torch.nn as nn
 import numpy as np
 from cs336_basics.tokenizer import *
 from tqdm import trange, tqdm
+import torch.nn.functional as F
+from typing import List
+
 
 class linear(nn.Module):
     """
@@ -548,6 +551,32 @@ class transformers_lm_muP(nn.Module):
             #probes=softmax(x,dim=-1)
 
         return probes
+
+
+class MoeLayer(nn.Module):
+    """
+    Mistral implementation of MoEs
+    """
+    def __init__(self, experts: List[nn.Module], gate: nn.Module, moe_args: MoeArgs):
+        super().__init__()
+        assert len(experts) > 0
+        self.experts = nn.ModuleList(experts)
+        self.gate = gate
+        self.args = moe_args
+
+    def forward(self, inputs: torch.Tensor) -> torch.Tensor:
+        gate_logits = self.gate(inputs)
+        weights, selected_experts = torch.topk(gate_logits, self.args.num_experts_per_tok)
+        weights = F.softmax(weights, dim=1, dtype=torch.float).to(inputs.dtype)
+        results = torch.zeros_like(inputs)
+        for i, expert in enumerate(self.experts):
+            batch_idx, nth_expert = torch.where(selected_experts == i)
+            results[batch_idx] += weights[batch_idx, nth_expert, None] * expert(inputs[batch_idx])
+        return results
+
+
+
+
 
 def decoding(x, model, max_tokens, tau, threshold,type):
     """ 
